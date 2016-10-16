74e509134c39247e8c72d920479fff51c11c7a9b
Hieu Nguyen <hnnguyen45@gmail.com>
2016-10-15 23:31:27 -0700

    fixed script

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-$(document).ready( function() {
+$(document).ready(function(){
 	var allergyChosen;
-
 	$('#searchButton').on('click', function (event) {	
 	    event.preventDefault();
 	    var keyword = $('#ingredient').val();
